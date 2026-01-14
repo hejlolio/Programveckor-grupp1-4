bf086866b4ef920b0f8b0d5d946669b2d8168224
ZeroSk1pper <nellie.sjodahl@elev.ga.lbs.se>
2026-01-14 08:59:02 +0100

    test

--- Assets/dropping.cs
+++ Assets/dropping.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
+
 public class dropping : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float timer = 0;
+    Rigidbody2D rb;
+    public GameObject square2;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        //hämta rigidbody
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer > 1 && timer < 1.5)
+        {
+            //velocity på rb
+            rb.linearVelocity
+
+
+        }
+
+
+        //när y-värdet ör samma som suqares y värde. Sätt this .SetActive(false) och square SetActive(true)
+
+
+        //om timer > 5 byt igen setActive på både. och ändr aposition för flaskan. nollställ velocity. 
     }
+
+}
 }
