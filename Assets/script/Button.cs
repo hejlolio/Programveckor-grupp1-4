7af585449cc8d8891d407f45d81c5149021a03c9
Aggebaggenn <august.engholm@elev.ga.lbs.se>
2026-01-14 17:06:14 +0100

    play button i main menu

---
+++ Assets/script/Button.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
