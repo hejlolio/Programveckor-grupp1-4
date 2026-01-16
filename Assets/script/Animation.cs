b18102875126edbe6c0862a38c28c7ab750d1e3d
Aggebaggenn <august.engholm@elev.ga.lbs.se>
2026-01-16 13:53:40 +0100

    animation script

---
+++ Assets/script/Animation.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    
+    [SerializeField] private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+            animator.SetBool("IsRunning", true);
+
+        if (Input.GetKey(KeyCode.A))
+            animator.SetBool("IsRunning", true);
+        transform.localScale = new Vector3(-1, 1, 1);
+    }
+}
